4401b149a1921b26fa69a89568445db020236144
Eric Connell <econnell@adt.com>
2017-01-10 23:30:01 -0700

    no classes

--- src/main/groovy/org/mangler/jenkins/Notification.groovy
+++ src/main/groovy/org/mangler/jenkins/Notification.groovy
@@ -2,29 +2,27 @@
 
 package org.mangler.jenkins;
 
-class Notification {
-    def notifyBuild(String buildStatus = 'STARTED', channel = '@econnell') {
-        // build status of null means successful
-        buildStatus =  buildStatus ?: 'SUCCESSFUL'
+def notifyBuild(String buildStatus = 'STARTED', channel = '@econnell') {
+    // build status of null means successful
+    buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
-        // Default values
-        def colorName = 'RED'
-        def colorCode = '#FF0000'
-        def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-        def summary = "${subject} branch: ${env.GIT_BRANCH} (${env.BUILD_URL})"
+    // Default values
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} branch: ${env.GIT_BRANCH} (${env.BUILD_URL})"
 
-        // Override default values based on build status
-        if (buildStatus == 'STARTED') {
-            color = 'YELLOW'
-                colorCode = '#FFFF00'
-        } else if (buildStatus == 'SUCCESSFUL') {
-            color = 'GREEN'
-                colorCode = '#00FF00'
-        } else {
-            color = 'RED'
-                colorCode = '#FF0000'
-        }
-
-        slackSend (color: colorCode, message: summary, channel: channel)
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+            colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL') {
+        color = 'GREEN'
+            colorCode = '#00FF00'
+    } else {
+        color = 'RED'
+            colorCode = '#FF0000'
     }
+
+    slackSend (color: colorCode, message: summary, channel: channel)
 }
